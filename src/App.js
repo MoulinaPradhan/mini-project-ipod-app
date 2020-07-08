9a7cd15fd9e7976774d5fc38afebe2262f23c692
moulina pradhan <pmoulina76@gmail.com>
2020-07-08 18:13:32 +0530

    add wheel

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Wheel from './wheel'
+
+function App() {
+  return (
+    <div className="App">
+  <Wheel/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/wheel.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+
+class Wheel extends  React.Component {
+    render(){
+        return(
+            <div className="wheel-container">
+            <div className="wheel">
+              <div id="menu" className="handle-btn">MENU</div>
+              <div id="forward" className="handle-btn">
+                <img src="https://image.flaticon.com/icons/svg/1/1824.svg"/>
+              </div>
+              <div id="backward" className="handle-btn">
+                <img src="https://image.flaticon.com/icons/svg/39/39395.svg"/>
+                </div>
+              <div id="pause" className="handle-btn">
+                <img src="https://image.flaticon.com/icons/svg/2404/2404317.svg"/>
+              </div>
+              <div className="main-btn"></div>
+            </div>
+          </div>
+        )
+    }
+}
+
+export default Wheel;
